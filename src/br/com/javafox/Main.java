0247b638cac8c0284665e6cba2af79b88cc2d112
Guerra-BR <guerinohenrique@gmail.com>
2023-11-20 16:29:31 -0300

    Primeiro commit em java

---
+++ src/br/com/javafox/Main.java
@@ -0,0 +1,14 @@
+package br.com.javafox;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+
+        if (args.length > 0){
+            System.out.println("Olá " + args[0]);
+            System.out.println("Finalmente voltei para esta IDE");
+            System.out.println("Senti bastante saudades, desde que programava bots para disrcod aqui...");
+        }
+    }
+}
